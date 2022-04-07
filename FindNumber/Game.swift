ee1d8a65537423cb2749f8427fdbc7622dce127d
podnachitana <t.zudina1@gmail.com>
2022-04-07 20:17:28 +0300

    add logic for game

--- FindNumber/Game.swift
+++ FindNumber/Game.swift
@@ -7,4 +7,56 @@
 
 import Foundation
 
+enum StatusGame{
+	case start
+	case win
+}
+
+class Game {
+	
+	struct Item {
+	var title:String
+	var isFound:Bool = false
+	}
+	
+	private let data = Array(1...99)
+	
+	var items:[Item] = []
+	
+	private var countItems:Int
+	
+	var nextItem:Item?
+	
+	var status:StatusGame = .start
+	
+	init(countItems:Int){
+		self.countItems = countItems
+		setupGame()
+	}
+	
+	private func setupGame() {
+		var digits = data.shuffled()
+		
+		while items.count < countItems {
+			let item = Item(title: String(digits.removeFirst()))
+			items.append(item)
+		}
+		
+		nextItem = items.shuffled().first
+	}
+	
+	func check(index:Int){
+		
+		if items[index].title == nextItem?.title{
+			items[index].isFound = true
+			nextItem = items.shuffled().first(where: { item -> Bool in
+				item.isFound == false
+			})
+		}
+		
+		if nextItem == nil{
+			status = .win
+		}
+	}
+}
 
--- FindNumber/GameViewController.swift
+++ FindNumber/GameViewController.swift
@@ -8,18 +8,45 @@
 import UIKit
 
 class GameViewController: UIViewController {
+	@IBOutlet var nextDigit: UILabel!
 	@IBOutlet var buttons: [UIButton]!
+	@IBOutlet weak var statusLabel: UILabel!
+	
+	
+	
+	lazy var game = Game(countItems: buttons.count)
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		setupScreen()
     }
     
 
 	@IBAction func pressButton(_ sender: UIButton) {
-		sender.isHidden = true
+		guard let buttonIndex = buttons.firstIndex(of: sender) else {return}
+		game.check(index: buttonIndex)
+		
+		updateUI()
 	}
 	
+	private func setupScreen() {
+		for index in game.items.indices{
+			buttons[index].setTitle(game.items[index].title, for: .normal)
+			buttons[index].isHidden = false
+		}
+		
+		nextDigit.text = game.nextItem?.title
+	}
+	
+	private func updateUI() {
+		for index in game.items.indices{
+			buttons[index].isHidden = game.items[index].isFound
+		}
+		nextDigit.text = game.nextItem?.title
+		
+		if game.status == .win{
+			statusLabel.text = "You win!"
+		}
 
+	}
 }
